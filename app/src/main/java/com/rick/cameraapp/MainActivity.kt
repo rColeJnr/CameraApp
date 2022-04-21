9660bd9b6a7ca3ee0127c63bea606d377eb07399
rcolejnr <ricardo.m.jnr@gmail.com>
2022-04-21 16:36:06 +0300

    camera layout fixed

--- app/src/main/java/com/rick/cameraapp/MainActivity.kt
+++ app/src/main/java/com/rick/cameraapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.rick.cameraapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.rick.cameraapp.databinding.ActivityMainBinding
@@ -16,15 +15,15 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+3
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+//        val navController = findNavController(R.id.navFragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard
+                R.id.navigation_gallery, R.id.navigation_camera
             )
         )
     }
@@ -37,7 +36,9 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (!CameraPermissionHelper.hasCameraPermission(this) || !CameraPermissionHelper.hasStoragePermission(this)) {
             CameraPermissionHelper.requestPermissions(this)
-        } else recreate()
+        } else recreate() /*If the hasCameraPermission
+and hasStoragePermission methods both return values of true, then the recreate method will reload the activity
+because the necessary user permissions have been granted.*/
     }
 }
 
